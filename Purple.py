60799a76a2e94acafe49f02d486b8c872a56fec7
Zachary Goodman <zjgoodman@utexas.edu>
2015-07-29 19:27:12 -0500

    ready to implement purple guess

--- Purple.py
+++ Purple.py
@@ -1,10 +1,14 @@
+from __future__ import division
 from PlayingCards import Card, Deck
 
 def guessBlack(deck) :
-	return deck.nextCard().red()
+	return deck.nextCard().black()
 
 def guessRed(deck) :
-	return deck.nextCard().black()
+	return deck.nextCard().red()
+
+def guessPurple(deck) :
+	return False #to be updated	
 
 def guess(color, deck) :
 	if color == 'r' :
@@ -14,13 +18,46 @@ def guess(color, deck) :
 	return guessPurple(deck)
 
 deck = Deck()
-print("guessing red")
-true  = 0
-false = 0
+print("Red:")
+success = 0
+failure = 0
 for i in range(0, 52) :
 	result = guess('r', deck)
-	print(" " + str(result))
-	if result :
-		true += 1
+	if result == True :
+		success += 1
 	else :
-		false += 1
+		failure += 1
+print(success)
+print(failure)
+print(str(success / 52) + "% success, " + str(failure / 52) + "% failure")
+
+print(" ")
+deck = Deck()
+print("Black:")
+success = 0
+failure = 0
+for i in range(0, 52) :
+	result = guess('b', deck)
+	if result == True :
+		success += 1
+	else :
+		failure += 1
+print(success)
+print(failure)
+print(str(success / 52) + "% success, " + str(failure / 52) + "% failure")
+
+print(" ")
+deck = Deck()
+print("Purple:")
+success = 0
+failure = 0
+for i in range(0, 26) :
+	result = guess('p', deck)
+	if result == True :
+		success += 1
+	else :
+		failure += 1
+print(success)
+print(failure)
+print(str(success / 26) + "% success, " + str(failure / 26) + "% failure")
+
